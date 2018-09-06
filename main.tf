d1c760eddddb81350cda2932791be63896139a60
Gearnode <friminb@gmail.com>
2018-09-06 15:56:12 +0200

    Fix index error

--- main.tf
+++ main.tf
@@ -17,13 +17,13 @@ limitations under the License.
 resource "aws_subnet" "main" {
   count             = "${length(var.cidr_blocks)}"
   vpc_id            = "${var.vpc_id}"
-  cidr_block        = "${element(var.cidr_blocks, index)}"
-  availability_zone = "${var.zones[index % length(var.zones)]}"
+  cidr_block        = "${element(var.cidr_blocks, count.index)}"
+  availability_zone = "${var.zones[count.index % length(var.zones)]}"
 
   tags = "${
     merge(
       var.tags,
-      map("Name", "${var.name}-${var.zones[index % length(var.zones)]}")
+      map("Name", "${var.name}-${var.zones[count.index % length(var.zones)]}")
     )
   }"
 }
